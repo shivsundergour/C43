4c8207e39895ba19ed1ae352a364ca9344bccf48
shivsundergour <81570170+shivsundergour@users.noreply.github.com>
2021-08-08 12:25:39 +0530

    Add files via upload

---
+++ System.js
@@ -0,0 +1,11 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode, enteredCode){
+        if (actualCode === enteredCode)
+        return true
+        else
+        return false
+    }
+}
